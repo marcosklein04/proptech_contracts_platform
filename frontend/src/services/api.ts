a431977a1f48270277f4d299d8c6a62b7d7c5cb3
marcosklein_ <marcosklein815@gmail.com>
2026-01-14 15:40:38 -0300

    Remove auth header for contracts API

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -7,8 +7,8 @@ export const api = axios.create({
   baseURL,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+//api.interceptors.request.use((config) => {
+//  const token = localStorage.getItem("token");
+//  if (token) config.headers.Authorization = `Bearer ${token}`;
+//  return config;
+//});
