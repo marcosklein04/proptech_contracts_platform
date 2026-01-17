ba2cb270074bbb5dd2aa4cb0fce4291e3b41d9cb
marcosklein_ <marcosklein815@gmail.com>
2026-01-17 14:17:49 -0300

    back

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -301,4 +301,5 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")), debug=True)
+    port = int(os.getenv("PORT", "10000"))
+    app.run(host="0.0.0.0", port=port, debug=False)
