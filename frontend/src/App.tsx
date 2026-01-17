d31528c16054b029d25701d4824ead3150b50d2a
marcosklein_ <marcosklein815@gmail.com>
2026-01-17 18:32:16 -0300

    Protect routes: require token to access dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        {/* Login siempre público */}
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Todo lo demás protegido */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Dashboard />} />
+          {/* Si tenés otras páginas, agregalas acá adentro */}
+          {/* <Route path="/contratos" element={<Contratos />} /> */}
+          {/* <Route path="/propiedades" element={<Propiedades />} /> */}
+        </Route>
 
         {/* fallback */}
         <Route path="*" element={<div style={{ padding: 24 }}>404 Not Found</div>} />
